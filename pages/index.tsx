bc7cc80084ddb72b4afefe6e433931278dae13be
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-25 11:09:02 +0100

    Designed Small Cards & stored their data locally

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { getLocalData } from "../lib/localdata";
 import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
+import SmallCard from "@/components/SmallCard";
+
+export type ExploreDataItem = {
+  img: string;
+  location: string;
+  distance: string;
+};
 
 const inter = Inter({ subsets: ["latin"] });
+const fetcher = (url: URL) => fetch(url).then((res) => res.json());
 
-export default function Home() {
+export default function Home({
+  exploreData,
+}: {
+  exploreData: ExploreDataItem[];
+}) {
   return (
     <>
       <Head>
@@ -17,6 +30,32 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData.map((item) => {
+              return (
+                <SmallCard
+                  key={item.location}
+                  img={item.img}
+                  distance={item.distance}
+                  location={item.location}
+                />
+              );
+            })}
+          </div>
+        </section>
+      </main>
     </>
   );
+}
+// next.js fn for server rendering
+export async function getStaticProps() {
+  const exploreData: string = await getLocalData("exploreData");
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
